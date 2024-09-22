11aebb966512a3a18551d94c204bbc9f06e956f8
jahedripon9 <jahedahmed3170@gmail.com>
2024-09-22 09:16:13 +0600

    update

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -4,16 +4,16 @@ import img4 from '../../assets/feedback-author-4.png'
 const Experience = () => {
   return (
     <div className='container mx-auto' id='experience'>
-        <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
     <h2 className="text-center text-4xl font-bold">Your Traveler's Experience</h2>
       <p className="text-center text-lg mt-4 mb-12 text-gray-500">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
       </p>
-    <div class="flex flex-wrap -m-4">
-      <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-        <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img1}/>
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img1}/>
           <br />
           <div className="rating">
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
@@ -23,15 +23,15 @@ const Experience = () => {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
     
 </div>
-          <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-          <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Amin Rahman</h2>
-          <p class="text-gray-500">Popular Customer</p>
+          <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+          <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Amin Rahman</h2>
+          <p className="text-gray-500">Popular Customer</p>
         </div>
       </div>
-      <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-        <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img3}/>
+      <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img3}/>
           <br />
           <div className="rating">
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
@@ -40,15 +40,15 @@ const Experience = () => {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
 </div>
-          <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-          <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Anwar Ali</h2>
-          <p class="text-gray-500">Popular Customer</p>
+          <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+          <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Anwar Ali</h2>
+          <p className="text-gray-500">Popular Customer</p>
         </div>
       </div>
-      <div class="lg:w-1/3 lg:mb-0 p-4">
-        <div class="h-full text-center">
-          <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img4}/>
+      <div className="lg:w-1/3 lg:mb-0 p-4">
+        <div className="h-full text-center">
+          <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={img4}/>
           <br />
           <div className="rating">
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
@@ -57,10 +57,10 @@ const Experience = () => {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
 </div>
-          <p class="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-          <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
-          <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Jamal Uddin</h2>
-          <p class="text-gray-500">Popular Customer</p>
+          <p className="leading-relaxed">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+          <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+          <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">Jamal Uddin</h2>
+          <p className="text-gray-500">Popular Customer</p>
         </div>
       </div>
     </div>
--- src/pages/TravelPlace/TravelPlace.jsx
+++ src/pages/TravelPlace/TravelPlace.jsx
@@ -10,113 +10,113 @@ import place_bg_8 from '../../assets/place-bg-8.jpg'
 const TravelPlace = () => {
   return (
     <div className='container mx-auto' id='place'>
-<section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
   <h2 className="text-center text-4xl font-bold">Your Travel Information</h2>
       <p className="text-center text-lg mt-4 mb-12 text-gray-500">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
       </p>
 
       
-    <div class="flex flex-wrap justify-center gap-6 mx-4">
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl ">
-        <a class="block relative h-48 rounded overflow-hidden">
+    <div className="flex flex-wrap justify-center gap-6 mx-4">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl ">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_1} />
         </a>
-        <div class="mt-4 ">
+        <div className="mt-4 ">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">India</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">India</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_2} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">India</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">India</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_3} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-11 rounded-tr-3xl rounded-bl-3xl0" src={place_bg_4} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_5} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_6} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_7} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_8} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_8} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
-      <div class="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/6 md:w-1/2 p-4 w-full shadow-xl rounded-tr-3xl rounded-bl-3xl">
+        <a className="block relative h-48 rounded overflow-hidden">
           <img alt="ecommerce" className="object-cover object-center w-full h-full block transition-transform duration-300 transform hover:scale-110 rounded-tr-3xl rounded-bl-3xl" src={place_bg_8} />
         </a>
-        <div class="mt-4">
+        <div className="mt-4">
            
-          <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p class="mt-1 font-bold">BDT-10,000/-</p>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1 font-bold">BDT-10,000/-</p>
         </div>
       </div>
     </div>
